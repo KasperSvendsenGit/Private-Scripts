a34321860b762df0587c85e5423f86f7def13575
Kasper Svendsen <ksvendsen99@gmail.com>
2022-12-25 19:12:49 +0100

    Update Split hashes and dedublicate.ps1

--- Split hashes and dedublicate.ps1
+++ Split hashes and dedublicate.ps1
@@ -1,12 +1,20 @@
 
 # Split hashes and passwords into seperate files
 
-$Infile = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-raw-output1.txt"
-$Outfilehashes = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-hashes-output1.txt"
-$OutfilePW = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-passwords-output1.txt"
+$Infile = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-raw-output4.txt"
+$fullhashlist = "D:\_\_Rendering\OneDrive_1_12-25-2022\pwned-passwords-sha1-trimmed_01.txt"
+
+$Outfilehashesfound = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-hashes-output4.txt"
+$Outfilehashesnotfound = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-hashes-output4.txt"
+$OutfilePW = "D:\_\_Rendering\OneDrive_1_12-25-2022\haveibeenpwned-passwords-output4.txt"
+
+
+
 
 $StreamReader = [System.IO.StreamReader]::new($Infile)
-$StreamWriterhashes =  [System.IO.StreamWriter]::new($Outfilehashes)
+$fullhashlistStreamReader = [System.IO.StreamReader]::new($fullhashlist)
+$StreamWriterhashesfound =  [System.IO.StreamWriter]::new($Outfilehashesfound)
+$StreamWriterhashesnotfound =  [System.IO.StreamWriter]::new($Outfilehashesnotfound)
 $StreamWriterPW =  [System.IO.StreamWriter]::new($OutfilePW)
 
 while (-not $StreamReader.EndOfStream) {
@@ -15,26 +23,30 @@ while (-not $StreamReader.EndOfStream) {
         $splitline = $line.Split(":")
         $splitline | ForEach-Object {
 		    if ($_.Length -eq 40) {
-			    $StreamWriterhashes.WriteLine([system.string]$_)
-		    }
-		    else {
-		    $StreamWriterPW.WriteLine([system.string]$_)
+                foreach ($fullline in $fullhashlistStreamReader.ReadLine()) {
+                    if ($fullline -eq $_) {
+                        # if the current string is found in both files, write to found
+                        $StreamWriterhashesfound.WriteLine([System.String]$_)
+                    }
+                    elseif ($fullline -ne $_) {
+                        # uf the current string is NOT found in both files, write to not found
+                        $StreamWriterhashesnotfound.WriteLine([system.string]$_)
+                    }
+                }    
             }
-		}
+	    }
+	    else {
+	        $StreamWriterPW.WriteLine([system.string]$_)
+        }
 	}
 }
 
+
+
+
 $StreamReader.Close()
-$StreamWriterhashes.Close()
+$StreamWriterhashesfound.Close()
 $StreamWriterPW.Close()
-
-
-
-
-
-
-
-
-
-
+$fullhashlistStreamReader.Close()
+$StreamWriterhashesnotfound.Close()
 
